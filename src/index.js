fd19eb6da81f40a06a65addd62449a44b5422ba3
ogneyar <ogneyar-ne@ya.ru>
2021-02-10 15:12:25 +0300

    eeeeee

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './styles/index.scss'
 
 let $h1 = document.getElementById("h1")
+let $head = document.getElementById("head")
 
 setTimeout(()=>{
     $h1.innerHTML = "Как Вам Parcel сер???"
+    $head.innerHTML = "Parcel testing..."
 }, 2000)
